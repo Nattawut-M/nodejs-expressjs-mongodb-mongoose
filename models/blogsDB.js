cdde2b939a3c6938c8dc75cff2cc26677d340491
Nattawut_Meesamsen <T.nattawut2020@hotmail.com>
2020-10-09 15:40:52 +0700

    [--] new CONST 'db' = mongoose.connection

--- models/blogsDB.js
+++ models/blogsDB.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const dbUrl = 'mongodb://localhost:27017/blogDB';
 
 // use 'mongoose' to connection database 'mongodb://localhost:27017/blogDB'
-const db = mongoose.connect(dbUrl, {
+mongoose.connect(dbUrl, {
    useNewUrlParser: true
 })
+const db = mongoose.connection;
