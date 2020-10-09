cc8569750f87dd12299169dcdaa3c2ccd8dd2a99
Nattawut_Meesamsen <T.nattawut2020@hotmail.com>
2020-10-09 15:37:07 +0700

    [models/blogsDB.js] use 'mongoose' to connection database

--- models/blogsDB.js
+++ models/blogsDB.js
@@ -2,3 +2,8 @@ const mongodb = require('mongodb');
 const mongoose = require('mongoose');
 // mongodb + host + port + databaseName
 const dbUrl = 'mongodb://localhost:27017/blogDB';
+
+// use 'mongoose' to connection database 'mongodb://localhost:27017/blogDB'
+const db = mongoose.connect(dbUrl, {
+   useNewUrlParser: true
+})
