2bbe6fcdf416940d506540a03106d58238b3ad79
Nattawut_Meesamsen <T.nattawut2020@hotmail.com>
2020-10-09 16:04:10 +0700

    [routes/blogs.js] import [models/blogsDB.js] and assign to const 'blogsDB'

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+// import [models/blogsDB.js]
+const blogsDB = require('../models/blogsDB');
+
 // Blogs Homepage
 router.get('/', (req, res, next) => {
    res.render('blogs/index', {hi: "from routes/blogs"});
